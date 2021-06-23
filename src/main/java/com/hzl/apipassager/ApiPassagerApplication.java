d1a9b0cf2be5a42e8a8527d6588c0cae1620e835
HeZeLiang <1416818344@qq.com>
2021-06-23 14:51:54 +0800

    第一次提交一下

---
+++ src/main/java/com/hzl/apipassager/ApiPassagerApplication.java
@@ -0,0 +1,15 @@
+package com.hzl.apipassager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiPassagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPassagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hzl/apipassager/ApiPassagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzl.apipassager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPassagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
